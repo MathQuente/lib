e88eca9430274762d26cc1f363c2f4b942678da5
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-06-11 17:50:19 -0300

    feat: create google auth route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,6 @@ import { AuthController } from '../controllers/auth.controller'
 import { AuthRepository } from '../repositories/auth.repository'
 import { AuthService } from '../services/auth.service'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { authMiddleware } from '../middleware/auth.middleware'
 
 export async function authRoutes(app: FastifyInstance) {
   const authRepository = new AuthRepository()
@@ -22,17 +21,34 @@ export async function authRoutes(app: FastifyInstance) {
       authController.loginHandler(request, reply)
     )
 
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/refresh',
-    {
-      onRequest: [authMiddleware]
-    },
-    async (request, reply) => authController.refreshTokenHandler(request, reply)
-  )
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/refresh', async (request, reply) =>
+      authController.refreshTokenHandler(request, reply)
+    )
 
   app
     .withTypeProvider<ZodTypeProvider>()
     .post('/logout', async (request, reply) =>
       authController.logoutHandler(request, reply)
     )
+
+  app.get('/google/callback', async (request, reply) => {
+    return authController.googleCallback(request, reply)
+  })
+
+  app.get('/google', async (request, reply) => {
+    const oauth2 = (request.server as any).googleOAuth2
+    oauth2.generateAuthorizationUri(
+      request,
+      reply,
+      (err: any, authorizationUrl: any) => {
+        if (err) {
+          request.log.error(err)
+          return reply.code(500).send('Erro ao gerar URL de autorização')
+        }
+        return reply.redirect(authorizationUrl)
+      }
+    )
+  })
 }
